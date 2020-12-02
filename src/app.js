09b7214214ee2983b5eb14ecc2315fdcee57497d
henryvalle <57874459+henryvalle@users.noreply.github.com>
2020-12-01 22:57:13 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,9 +4,16 @@ const logger = require('./utils/logger');
 
 const app = express();
 
-app.route('/prueba').get((req, res) => {
-    logger.info('Usuario autenticado');
+//app.route('/prueba').get((req, res) => {
+//    logger.info('Usuario autenticado');
+//    res.send('Perfil de Usuario');
+//})
+
+app.get('/prueba', function(req, res) {
+ logger.info('Usuario autenticado');
     res.send('Perfil de Usuario');
-})
+  res.send(respuesta);
+ });
+
 
 app.listen(8081, () => logger.info('Server is running'));
